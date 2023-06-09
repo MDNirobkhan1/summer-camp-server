8fd5962c2ce30b00887dc83231f5898dbd111f2a
MDNirobkhan1 <nirob937612@gmail.com>
2023-06-09 10:29:54 +0600

    select data get

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const app= express();
+const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -15,53 +15,61 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const clessesCollection =client.db('photography-school').collection('clesses');
-    const cartCollection =client.db('photography-school').collection('carts');
+        const clessesCollection = client.db('photography-school').collection('clesses');
+        const cartCollection = client.db('photography-school').collection('carts');
 
-    app.get('/clesses', async(req, res)=>{
-        const result= await clessesCollection.find().toArray();
-        res.send(result)
-    })
+        app.get('/clesses', async (req, res) => {
+            const result = await clessesCollection.find().toArray();
+            res.send(result)
+        })
 
 
-    // cart collection
+        // cart collection
 
-    app.post('/carts', async(req,res)=>{
-        const item = req.body;
-        console.log(item);
-        const result = await cartCollection.insertOne(item);
-        res.send(result)
-    })
+        app.get('/carts', async (req, rea) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([])
+            }
+            const query = { email: email };
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        app.post('/carts', async (req, res) => {
+            const item = req.body;
+            console.log(item);
+            const result = await cartCollection.insertOne(item);
+            res.send(result)
+        })
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
 
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('summer is running')
 });
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`summer port is running Port :${port}`);
 })
 
